7448197af2d0ce6c85f2a1c87064a6e49a55c22e
swapnil.gorey <swapnil.gorey@envrmnt.com>
2021-05-17 16:48:22 -0400

    Added Delete Functionality

--- public/view.py
+++ public/view.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template,request,flash
+from flask import Blueprint, render_template,request,flash,redirect,url_for
 from flask_login import login_required,current_user
 from .models import Post, User
 from public import db
@@ -9,18 +9,24 @@ pages = Blueprint('pages',__name__)
 def home():
     posts=Post.query.all()
     users=User.query.all()
-    for post in posts:
-        print(post.postData)
+    # for post in posts:
+    #     print(post.postData)
     return render_template("home.html",user=current_user,posts=posts, users=users)
 
-# @pages.route('/deletePost',methods=['GET','DELETE'])
-# @login_required
-# def deletePost():
-#
-#         id=request.form.get(id)
-#         post=Post.query.all(id=id)
-#         print (post)
-#     return render_template("addPost.html", user=current_user)
+@pages.route('/deletePost/<int:post_id>',methods=['GET','DELETE'])
+@login_required
+def deletePost(post_id):
+
+    print (post_id)
+    deletepost=Post.query.filter_by(id=post_id).first()
+    db.session.delete(deletepost)
+    db.session.commit()
+    return redirect(url_for("pages.home"))
+# return render_template("/", user=current_user)
+        # id=request.form.get(id)
+        # post=Post.query.all(id=id)
+        # # print (post)
+
 
 @pages.route('/addPost',methods=['GET','POST'])
 @login_required
